2a2e95c5d13eac61d38e236bd406d47ad84e3d87
Simon <simonandro@github.com>
2021-10-28 19:39:43 +0800

    passing parameters to a module during insertion

---
+++ moduleparameters/modparams.c
@@ -0,0 +1,42 @@
+
+//modparams.c
+/*
+ * Brief Description:
+ */
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+#define OUR_MODNAME    "modparams"
+MODULE_AUTHOR("Simon");
+MODULE_DESCRIPTION("Passing a parameter to a module");
+MODULE_LICENSE("Dual MIT/GPL");
+MODULE_VERSION("0.1");
+
+/* Module parameters */
+static int mp_debug_level;
+module_param(mp_debug_level, int, 0660);
+MODULE_PARM_DESC(mp_debug_level,
+"Debug level [0-2]; 0 => no debug messages, 2 => high verbosity");
+
+static char *mp_strparam = "My string param";
+module_param(mp_strparam, charp, 0660);
+MODULE_PARM_DESC(mp_strparam, "A demo string parameter");
+
+static int __init modparams1_init(void)
+{
+	pr_info("%s: inserted\n", OUR_MODNAME);
+	pr_info("module parameters passed: mp_debug_level=%d mp_strparam=%s\n",
+		mp_debug_level, mp_strparam);
+	return 0;		/* success */
+}
+
+static void __exit modparams1_exit(void)
+{
+	pr_info("module parameters passed: mp_debug_level=%d mp_strparam=%s\n",
+		mp_debug_level, mp_strparam);
+	pr_info("%s: removed\n", OUR_MODNAME);
+}
+
+module_init(modparams1_init);
+module_exit(modparams1_exit);
